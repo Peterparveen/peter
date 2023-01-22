a2074d5271b2037b75d55f27be34e8a2e10c4ea8
Peterpraveen <praveenteckie007@gmail.com>
2023-01-22 20:06:02 +0530

    First time push

---
+++ src/test/java/org/sample/PgitHub.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class PgitHub {
+	 private void tc1() {
+		System.out.println("praveen");
+
+	}
+	 private void tc2() {
+			System.out.println("sudha");
+
+		}
+	 private void tc3() {
+		 System.out.println("subesh");
+		}
+
+}
